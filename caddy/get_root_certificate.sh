0837f19aa8c92aa44236f73952aae5f75950ce16
tsi <timo42@proton.me>
2025-03-01 06:34:06 +0100

    Add PROD=1 functionality and compile Caddy statically
    By specifying PROD=1 in the .env, the Caddyserver will set specific variable s.t. the Caddyfile template will be substituted differently (namely: with prod domain)

--- caddy/get_root_certificate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-docker exec caddy cat /data/caddy/pki/authorities/local/root.crt
-
-# Redirect out to a file (i.e. root.crt)
-# Then use like this with curl: curl --cacert root.crt https://localhost
-# Or install it in your operating system
-# Related: https://tech.surveypoint.com/tips/browser-trust-caddy-ssl-certs/
