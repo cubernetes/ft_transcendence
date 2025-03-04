31236eb090a1532075691f1c723b51ecc346d060
darrenkuro <odon5ht@gmail.com>
2025-03-04 20:33:32 +0100

    Add esbuild file

---
+++ backend/build.js
@@ -0,0 +1,22 @@
+import * as esbuild from "esbuild";
+
+const watch = process.argv.includes("--watch");
+
+const context = await esbuild.context({
+  entryPoints: ["src/server.ts"],
+  bundle: true,
+  platform: "node",
+  target: ["node20"],
+  outdir: "dist",
+  format: "esm",
+  sourcemap: true,
+  packages: "external",
+});
+
+if (watch) {
+  await context.watch();
+  console.log("Watching...");
+} else {
+  await context.rebuild();
+  await context.dispose();
+}
